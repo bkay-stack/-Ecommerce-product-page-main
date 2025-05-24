52a4c375d7e8e8e46e64640a3a585341c9b40471
bkay-stack <kevinbest1900@gmail.com>
2025-05-24 10:42:28 +0100

    slider

--- src/components/image-carousel/Carousel.tsx
+++ src/components/image-carousel/Carousel.tsx
@@ -3,6 +3,9 @@ import "./carosusel.styles.css";
 import { productImages } from "../../data/productImages";
 import arrowIMGleft from "../../assets/images/icon-next.svg";
 import arrowIMGright from "../../assets/images/icon-previous.svg";
+import plusIcon from "../../assets/images/icon-plus.svg";
+import minusIcon from "../../assets/images/icon-minus.svg";
+import cartIcon from "../../assets/images/icon-cart.svg";
 export const Carousel = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
@@ -16,64 +19,142 @@ export const Carousel = () => {
   };
 
   return (
-    <div className="sneakers-showcase">
-      <div className="sneaker-company">
-        <h1>Sneaker Company</h1>
-        <h2>Fall Limited Edition Sneakers</h2>
-        <p>
-          These low-profile sneakers are your perfect casual wear companion.
-          Featuring a durable rubber outer sole, they’ll withstand everything
-          the weather can offer.
-        </p>
-        <div className="price">
-          <span>$125.00</span>
-          <span className="discount">50%</span>
+    <>
+      {/* Large Screen View */}
+      <div className="sneakers-showcase container larger-screen">
+        {/* Sneaker Content */}
+        <div className="sneaker-content ">
+          <h3>Sneaker Company</h3>
+          <h2>Fall Limited Edition Sneakers</h2>
+          <p>
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, they’ll withstand everything
+            the weather can offer.
+          </p>
+          <div className="price-content">
+            <div className="price">
+              <h4 className="init-price">$125.00</h4>
+              <span className="discount">50%</span>
+            </div>
+            <span className="old-price">$250.00</span>
+          </div>
+
+          {/* Add to Cart Section */}
+          <div className="add-to-cart">
+            <div className="quantity">
+              <img src={minusIcon} alt="" className="minus" />
+              <span className="number">0</span>
+              <img src={plusIcon} alt="" className="plus" />
+            </div>
+            {/* <> */}
+            <div className="cart">
+              <button className="add-to-cart-btn">
+                <div className="cart-icon">
+                  <img src={cartIcon} alt="" />
+                </div>
+                <span>Add to cart</span>
+              </button>
+            </div>
+            {/* </> */}
+          </div>
         </div>
-        <div className="old-price">$250.00</div>
-        <div className="add-to-cart">
-          <button className="add-to-cart-btn">
+
+        {/* Image Carousel */}
+        <div className="carousel">
+          <div className="arrow-left">
             <img
-              src="../../assets/images/icon-cart.svg"
-              alt="cart"
-              className="cart-icon"
+              src={arrowIMGright}
+              alt=""
+              className="arrow arrow-left"
+              onClick={() => handlePrev()}
             />
-            Add to cart
-          </button>
-          <div className="quantity">
-            <button className="minus">-</button>
-            <span className="quantity-number">0</span>
-            <button className="plus">+</button>
+          </div>
+          {productImages.map((img, index) => (
+            <img
+              key={img.id}
+              src={img.src}
+              alt={img.alt}
+              className={`${index === currentIndex ? "active" : "hidden"}`}
+            />
+          ))}
+          <div className="arrow-right">
+            <img
+              src={arrowIMGleft}
+              alt=""
+              className="arrow "
+              onClick={() => handleNext()}
+            />
           </div>
         </div>
       </div>
 
-      {/* Image Carousel */}
-      <div className="carousel">
-        <div className="arrow-left">
-          <img
-            src={arrowIMGright}
-            alt=""
-            className="arrow arrow-left"
-            onClick={() => handlePrev()}
-          />
+      {/* Mobile View */}
+      <div className="sneakers-showcase ">
+        {/* Sneaker Content */}
+        <div className="sneaker-content ">
+          <h3>Sneaker Company</h3>
+          <h2>Fall Limited Edition Sneakers</h2>
+          <p>
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, they’ll withstand everything
+            the weather can offer.
+          </p>
+          <div className="price-content">
+            <div className="price">
+              <h4 className="init-price">$125.00</h4>
+              <span className="discount">50%</span>
+            </div>
+            <span className="old-price">$250.00</span>
+          </div>
+
+          {/* Add to Cart Section */}
+          <div className="add-to-cart">
+            <div className="quantity">
+              <img src={minusIcon} alt="" className="minus" />
+              <span className="number">0</span>
+              <img src={plusIcon} alt="" className="plus" />
+            </div>
+            {/* <> */}
+            <div className="cart">
+              <button className="add-to-cart-btn">
+                <div className="cart-icon">
+                  <img src={cartIcon} alt="" />
+                </div>
+                <span>Add to cart</span>
+              </button>
+            </div>
+            {/* </> */}
+          </div>
         </div>
-        {productImages.map((img, index) => (
-          <img
-            key={img.id}
-            src={img.src}
-            alt={img.alt}
-            className={`${index === currentIndex ? "active" : "hidden"}`}
-          />
-        ))}
-        <div className="arrow-right">
-          <img
-            src={arrowIMGleft}
-            alt=""
-            className="arrow "
-            onClick={() => handleNext()}
-          />
+
+        {/* Image Carousel */}
+        <div className="carousel">
+          <div className="arrow-left">
+            <img
+              src={arrowIMGright}
+              alt=""
+              className="arrow arrow-left"
+              onClick={() => handlePrev()}
+            />
+          </div>
+          {productImages.map((img, index) => (
+            <img
+              key={img.id}
+              src={img.src}
+              alt={img.alt}
+              className={`${index === currentIndex ? "active" : "hidden"}`}
+            />
+          ))}
+          <div className="arrow-right">
+            <img
+              src={arrowIMGleft}
+              alt=""
+              className="arrow "
+              onClick={() => handleNext()}
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
